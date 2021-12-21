0f0855ae6da89506eb000ea342b8d41899704072
Kyriakos Koutroulakis <koutroulakisk@gmail..com>
2021-12-21 19:01:37 +0200

    finished cryptocurrencies/homepage -- add searching bar on cryptocurrencies

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+  'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+  'x-rapidapi-key': 'c03c52998bmshcba0f3dea4a1f6ep1d2a01jsn1f33ac0f1df9'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`)
+    })
+  })
+});
+
+export const {
+  useGetCryptosQuery,
+} = cryptoApi;

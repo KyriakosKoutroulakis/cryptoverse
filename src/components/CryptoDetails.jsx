bb44644433eaf831f0d3e2bb65f674227ac75997
Kyriakos Koutroulakis <koutroulakisk@gmail..com>
2022-02-07 14:14:38 +0200

    Showing line chart - dates displayed wrong

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -14,11 +14,11 @@ const { Option } = Select;
 
 const CryptoDetails = () => {
   const { coinId } = useParams();
-  const [timeperiod, setTimeperiod] = useState('7d');
+  const [timePeriod, setTimePeriod] = useState('7d');
   const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
-  const { data: coinHistory} = useGetCryptoHistoryQuery({ coinId, timeperiod }); 
+  const { data: coinHistory} = useGetCryptoHistoryQuery({ coinId, timePeriod }); 
   const cryptoDetails = data?.data?.coin;
-
+  
   if (isFetching) return <Loader />;
   
   const time = ['3h', '24h', '7d', '30d', '3m', '1y', '3y', '5y'];
@@ -47,7 +47,7 @@ const CryptoDetails = () => {
         </Title>
         <p>{cryptoDetails.name} live price in US Dollar (USD). View value statistics, market cap and supply.</p>
       </Col>
-      <Select defaultValue="7d" className="select-timeperiod" placeholder="Select Timeperiod" onChange={(value) => setTimeperiod(value)}>
+      <Select defaultValue="7d" className="select-timeperiod" placeholder="Select Timeperiod" onChange={(value) => setTimePeriod(value)}>
         {time.map((date) => <Option key={date}>{date}</Option>)}
       </Select>
       <LineChart coinHistory={coinHistory} currentPrice={millify(cryptoDetails?.price)} coinName={cryptoDetails?.name} />
--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import { 
+  Chart as ChartJS, 
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Legend,
+  Title,
+  Tooltip,
+} from 'chart.js';
 import { Line } from 'react-chartjs-2';
 import { Col, Row, Typography } from 'antd';
 
-const { Title } = Typography;
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Legend,
+  Title,
+  Tooltip
+);
 
 const LineChart = ({ coinHistory, currentPrice, coinName }) => {
   const coinPrice = [];
@@ -10,44 +28,44 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
   
   for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
     coinPrice.push(coinHistory?.data?.history[i].price);
+  }
+
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
     coinTimestamp.push(new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString());
   }
-  
-  // const data = {
-  //   labels: coinTimestamp,
-  //   datasets: [
-  //     {
-  //       label: 'Price In USD',
-  //       data: coinPrice,
-  //       fill: false,
-  //       backgroundColor: '#0071bd',
-  //       borderColor: '#0071bd',
-  //     },
-  //   ],
-  // };
 
-  // const options = {
-  //   scales: {
-  //     yAxes: [
-  //       {
-  //         ticks: {
-  //           beginAtZero: true,
-  //         },
-  //       },
-  //     ],
-  //   },
-  // };
+  console.log(coinTimestamp);
+  const data = {
+    labels: coinTimestamp,
+    datasets: [
+      {
+        label: "Price in USD",
+        data: coinPrice,
+        fill: false,
+        borderColor: '#0071bd',
+        backgroundColor: '#0071bd',
+      },
+    ],
+  };
+
+  const options = {
+    plugins: {
+      title: { 
+        display: true,
+        text: `${coinName} Price Chart`
+      }
+    }
+  };
 
   return (
     <>
       <Row className="chart-header">
-        <Title level={2} className="chart-title">{coinName} Price Chart </Title>
+        <Typography.Title level={5} className="price-change">Change: {coinHistory?.data?.change}%</Typography.Title>
         <Col className="price-container">
-          <Title level={5} className="price-change">Change: {coinHistory?.data?.change}%</Title>
-          <Title level={5} className="current-price">Current {coinName} Price: $ {currentPrice}</Title>
+          <Typography.Title level={5} className="current-price">Current {coinName} Price: $ {currentPrice}</Typography.Title>
         </Col>
       </Row>
-      {/* <Line data={data} options={options} /> */}
+      <Line options={options} data={data} />
     </>
   );
 };

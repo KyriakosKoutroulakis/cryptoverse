5dc3d34528ca83b8386ea508c67e69773d3c757d
Kyriakos Koutroulakis <koutroulakisk@gmail..com>
2022-02-15 15:22:19 +0200

    .env added

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,14 +1,15 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoApiHeaders = {
-  'x-access-token': process.env.COINRANKING_API_KEY,
+  'x-rapidapi-host': process.env.REACT_APP_COINRANKING_RAPIDAPI_HOST,
+  'x-rapidapi-key': process.env.REACT_APP_COINRANKING_API_KEY
 }
 
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
 
 export const cryptoApi = createApi({
   reducerPath: 'cryptoApi',
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.COINRANKING_API_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_COINRANKING_API_URL }),
   endpoints: (builder) => ({
     getCryptos: builder.query({
       query: (count) => createRequest(`/coins?limit=${count}`),
